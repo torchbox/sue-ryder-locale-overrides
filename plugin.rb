6edad960da0a2063fdc142175fbef851eb7ea58e
Alex Bridge <alex.bridge@torchbox.com>
2019-11-27 17:06:07 +0000

    Add override for 404 page template to add a login link when applicable

--- plugin.rb
+++ plugin.rb
@@ -1,4 +1,8 @@
 # name: sueryder-locale-overrides
 # about: Provides overrides to locale configuration
-# version: 0.0.3
+# version: 0.0.4
 # authors: Alex Bridge
+
+after_initialize do
+  ApplicationController.prepend_view_path("plugins/sue-ryder-locale-overrides/app/views")
+end
