dbd10d9bbb9efaf29139c583e76951781f9f1b31
Dan Braghiș <dan.braghis@torchbox.com>
2020-06-23 16:13:34 +0000

    Add GH repo as version URL

--- plugin.rb
+++ plugin.rb
@@ -1,7 +1,8 @@
 # name: sueryder-locale-overrides
 # about: Provides overrides to locale configuration
 # version: 0.0.5
-# authors: Alex Bridge
+# authors: Alex Bridge, Dan Braghis
+# version url: https://github.com/torchbox/sue-ryder-locale-overrides
 
 after_initialize do
   begin
