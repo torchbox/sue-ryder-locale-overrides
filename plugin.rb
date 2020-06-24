17df4e3f3831f1e3617c30827ad4c5fe986e25c4
Dan Braghis <dan.braghis@torchbox.com>
2020-06-24 09:50:26 +0100

    Revert view path change

--- plugin.rb
+++ plugin.rb
@@ -1,12 +1,12 @@
 # name: sueryder-locale-overrides
 # about: Provides overrides to locale configuration
-# version: 0.0.5
+# version: 0.0.6
 # authors: Alex Bridge, Dan Braghis
 # version url: https://github.com/torchbox/sue-ryder-locale-overrides
 
 after_initialize do
   begin
-    ApplicationController.prepend_view_path("plugins/locale-overrides/app/views")
+    ApplicationController.prepend_view_path("plugins/sue-ryder-locale-overrides/app/views")
   rescue
   end
 end
